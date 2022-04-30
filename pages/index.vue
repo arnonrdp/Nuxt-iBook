adc3737d2951ecd9283adaf69713ae1063a4bde8
Arnon <arnonrdp@gmail.com>
2022-04-30 13:51:00 -0300

    Bóson de Breakpoint

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,23 @@
 <template>
-  <div class="box"></div>
+  <div class="wrapper">
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.box {
-  width: 300px;
-  height: 300px;
-  background-color: color('dark');
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 200px;
+  grid-auto-flow: column;
+  grid-gap: 0.5rem;
+  @include screen('small', 'medium') {
+    grid-template-columns: 1fr;
+  }
+  .box {
+    background-color: color('dark', 'darkest');
+  }
 }
 </style>
