963d731068639d47c351c70122fa2468837ce366
Arnon <arnonrdp@gmail.com>
2022-05-01 13:51:00 -0300

    Bóson de Container

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,12 @@
 <template>
   <div class="wrapper">
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
+    <h1>Home Page</h1>
   </div>
 </template>
 
-<style lang="scss" scoped>
-.wrapper {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: 200px;
-  grid-auto-flow: column;
-  grid-gap: 0.5rem;
-  @include screen('small', 'medium') {
-    grid-template-columns: 1fr;
-  }
-  .box {
-    background-color: color('dark', 'darkest');
-  }
-}
-</style>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  layout: 'ibook',
+})
+</script>
