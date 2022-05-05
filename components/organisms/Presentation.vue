67d17fc11b5c02618c9e0e7d2e64b7a1155d8398
Arnon <arnonrdp@gmail.com>
2022-05-05 13:51:00 -0300

    Organism Presentation

---
+++ components/organisms/Presentation.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="presentation">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis facilis voluptatibus, autem amet dignissimos esse eligendi doloribus
+      consectetur voluptates sunt quis ratione, quam odit consequuntur nisi voluptate non totam. Mollitia. Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Fuga modi, quo incidunt ipsam laudantium deserunt pariatur harum deleniti perspiciatis illo autem
+      quaerat vitae ipsa, et, in natus sint doloremque fugit?
+    </p>
+
+    <img src="@/assets/img/presentation.svg" alt="Apresentação do iBook">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+
+})
+</script>
+
+<style scoped></style>
