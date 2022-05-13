fe3ac303c51f37c8707a59c4902bfbd47cf1c618
Arnon <arnonrdp@gmail.com>
2022-05-13 13:51:00 -0300

    Página de detalhes

---
+++ pages/book/_id.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <BookDetailsTemplate />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { books } from '@/store'
+
+export default Vue.extend({
+  layout: 'ibook',
+  async asyncData({ params }) {
+    await books.show({ id: params.id as any })
+  },
+  head() {
+    return {
+      title: books.$single.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: books.$single.description,
+        },
+      ],
+    }
+  },
+})
+</script>
+
+<style scoped></style>
